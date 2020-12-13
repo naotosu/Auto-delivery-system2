81a7d08e90d57cff66b1f0bae74d5839020062a6
naotosu <re13brb26l28@hotmail.com>
2020-12-13 10:26:20 +0900

    Dockerfile暫定修正

--- ecr-push.sh
+++ ecr-push.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 aws ecr get-login-password --region ap-northeast-1 | docker login --username $AWS_USERNAME --password-stdin $ECR_ADDRESS
-docker-compose up -d --build
+docker build -t auto-delivery-system2_web ./infra/nginx/
 docker tag nginx:1.18-alpine $ECR_ADDRESS/laravel-app/nginx:latest
 docker push $ECR_ADDRESS/laravel-app/nginx:latest
+docker build -t auto-delivery-system2_app ./infra/php/
 docker tag auto-delivery-system2_app $ECR_ADDRESS/laravel-app/php-fpm:latest
 docker push $ECR_ADDRESS/laravel-app/php-fpm:latest
