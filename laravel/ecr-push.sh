6a0e308f5bc999e2136c6cfbd20c1cc95602b903
naotosu <re13brb26l28@hotmail.com>
2020-12-14 20:39:16 +0900

    dockerfile_php-fmp前準備

--- laravel/ecr-push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-aws ecr get-login-password --region ap-northeast-1 | docker login --username $AWS_USERNAME --password-stdin $ECR_ADDRESS
-docker build -t laravel-app/nginx ../infra/nginx/
-docker tag laravel-app/nginx $ECR_ADDRESS/laravel-app/nginx:latest
-docker push $ECR_ADDRESS/laravel-app/nginx:latest
-docker build -t laravel-app/php-fpm ../infra/php2/
-docker tag laravel-app/php-fpm $ECR_ADDRESS/laravel-app/php-fpm:latest
-docker push $ECR_ADDRESS/laravel-app/php-fpm:latest
