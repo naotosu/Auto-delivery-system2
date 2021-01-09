52b7732c7c88dd4b78cf58aa9e24792df3b3f34a
naotosu <re13brb26l28@hotmail.com>
2021-01-09 13:34:39 +0900

    パラメーターストア変更andOrderImtes日付変更

--- laravel/database/factories/OrderItemFactory.php
+++ laravel/database/factories/OrderItemFactory.php
@@ -9,7 +9,7 @@ use App\Models\Item;
 $factory->define(App\Models\OrderItem::class, function (Faker $faker) {
     return [
     	'order_id' => $faker->numberBetween($min = 1, $max = 3),
-    	'ship_date' => $faker->dateTimeBetween('2020-11-17', '2020-12-27'),
+    	'ship_date' => $faker->dateTimeBetween('2021-1-13', '2021-3-31'),
 		'weight' => $faker->numberBetween($min = 2000, $max = 20000),
         'item_code' => function() {
     	return Item::all()->random()->item_code;
