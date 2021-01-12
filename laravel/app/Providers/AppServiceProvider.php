e11414d7458c18083d02d73d3a97a9580f6c5aa2
naotosu <re13brb26l28@hotmail.com>
2021-01-12 20:02:24 +0900

    https化暫定対応_取り止め

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -24,7 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $is_production = env('APP_ENV') === 'production' ? true : false;
-        View::share('is_production',$is_production);       
+        Schema::defaultStringLength(191);        
     }
 }
